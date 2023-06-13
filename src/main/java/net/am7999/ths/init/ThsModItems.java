f46c19bbffd9fa538265415855e97cfe461502ae
AM7999 <AMartin12122019@Outlook.com>
2023-06-12 17:52:57 -0700

    Lots and lots of improvements
    texture updates courtesy of SquidDogTheLatexBoi and some new things added in general

--- src/main/java/net/am7999/ths/init/ThsModItems.java
+++ src/main/java/net/am7999/ths/init/ThsModItems.java
@@ -4,16 +4,13 @@
  */
 package net.am7999.ths.init;
 
+import net.am7999.ths.item.*;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.Item;
 
-import net.am7999.ths.item.SocksColorLimeItem;
-import net.am7999.ths.item.SocksColorPinkItem;
-import net.am7999.ths.item.SocksColorWhiteItem;
-import net.am7999.ths.item.CottonItem;
 import net.am7999.ths.ThsMod;
 
 public class ThsModItems {
@@ -22,4 +19,5 @@ public class ThsModItems {
 	public static final RegistryObject<Item> COTTON = REGISTRY.register("cotton", () -> new CottonItem());
 	public static final RegistryObject<Item> SOCKS_COLOR_PINK_LEGGINGS = REGISTRY.register("socks_color_pink", () -> new SocksColorPinkItem.Leggings());
 	public static final RegistryObject<Item> SOCKS_COLOR_LIME_LEGGINGS = REGISTRY.register("socks_color_lime", () -> new SocksColorLimeItem.Leggings());
+	public static final RegistryObject<Item> SOCKS_COLOR_ORANGE_LEGGINGS = REGISTRY.register("socks_color_orange", () -> new SocksColorOrangeItem.Leggings());
 }
